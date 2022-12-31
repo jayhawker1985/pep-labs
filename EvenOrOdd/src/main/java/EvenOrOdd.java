8c016f6564497ae4d7429ede89c6f0601a6a5d01
jayhawker1985 <hermanbarber60@gmail.com>
2022-12-31 01:18:57 +0000

    Saved progress on EvenOrOdd 2022-12-31 01:18:57 +0000

--- EvenOrOdd/src/main/java/EvenOrOdd.java
+++ EvenOrOdd/src/main/java/EvenOrOdd.java
@@ -14,9 +14,11 @@ public class EvenOrOdd {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        if(n % 2 == 0){
+        
+        if (n % 2 == 0) {
             return "Even";
         }
+
         return "Odd";
     }
 }
